207c4f6a53e309f6516bd3e9151a5eb88f8c9b89
Radosław Siek <Borderlandsmaniak@gmail.com>
2023-04-10 13:29:55 +0200

    10.04

--- index.js
+++ index.js
@@ -1,20 +1,35 @@
 // JavaScript Event Listeners
 
-const viewOne = document.querySelector('#view1');
-const viewTwo = document.querySelector('#view2');
-const viewThree = document.querySelector('#view3');
-const h2 = document.querySelector('h2');
-
 // Syntax: addEventListener(event, function, useCapture)
 
-const doSomething = () => {
-	alert('Doing something');
-};
-
-h2.addEventListener('click', doSomething, false);
-h2.removeEventListener('click', doSomething, false);
-
-h2.addEventListener('click', function (e) {
-	console.log(e.target);
-	e.target.textContent = 'Wow';
+document.addEventListener('readystatechange', (e) => {
+	if (e.target.readyState === 'complete') {
+		console.log('readyState: complete');
+		initApp();
+	}
 });
+
+const initApp = () => {
+	const viewOne = document.querySelector('#view1');
+	const viewTwo = document.querySelector('#view2');
+	const viewThree = document.querySelector('#view3');
+	const h2 = document.querySelector('h2');
+
+	viewOne.addEventListener('click', (e) => {
+		viewOne.style.backgroundColor = 'red';
+	});
+
+	viewTwo.addEventListener('click', (e) => {
+		e.stopPropagation();
+		e.target.style.backgroundColor = 'green';
+	});
+
+	viewThree.addEventListener('click', (e) => {
+		e.stopPropagation();
+		e.target.style.backgroundColor = 'purple';
+	});
+
+	h2.addEventListener('click', (e) => {
+		e.target.textContent = 'Heading clicked';
+	});
+};
