ca3579df312c009c66cb634e5a6b5aa09e43b36b
Radosław Siek <Borderlandsmaniak@gmail.com>
2023-04-11 18:27:19 +0200

    Finished modules lesson

--- js/index.js
+++ js/index.js
@@ -1,20 +1,11 @@
-// Web Storage API
+// Modules
+import * as Guitars from './guitars.js';
+import User from './user.js';
 
-// Not part of the DOM - refers to the Window API
-// Avialable to JS via global variable: window
+const me = new User('email.com', 'Emilia');
+console.log(me);
+console.log(me.greeting());
 
-// We do not have to type window. It is implied:
-
-const myArray = ['eat', 'sleep', 'code'];
-const myObject = {
-	name: 'Emilia',
-	hobbies: ['eat', 'sleep', 'code'],
-	logName: function () {
-		console.log(`Hi, my name is ${this.name}.`);
-	},
-};
-
-localStorage.setItem('myLocalStore', JSON.stringify(myArray));
-const key = localStorage.key(0);
-const myLocalData = JSON.parse(localStorage.getItem('myLocalStore'));
-console.log(key);
+console.log(Guitars.playGuitar());
+console.log(Guitars.shredding());
+console.log(Guitars.plucking());
