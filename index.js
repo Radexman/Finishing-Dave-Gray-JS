18f3f07d5803dfca77c0c89da5923134459d85b7
Radosław Siek <Borderlandsmaniak@gmail.com>
2023-04-11 16:53:31 +0200

    11.04

--- index.js
+++ index.js
@@ -14,22 +14,32 @@ const initApp = () => {
 	const viewTwo = document.querySelector('#view2');
 	const viewThree = document.querySelector('#view3');
 	const h2 = document.querySelector('h2');
+	const nav = document.querySelector('.navbar');
 
 	viewOne.addEventListener('click', (e) => {
-		viewOne.style.backgroundColor = 'red';
+		viewOne.classList.toggle('darkblue');
+		viewOne.classList.toggle('purple');
 	});
 
 	viewTwo.addEventListener('click', (e) => {
-		e.stopPropagation();
-		e.target.style.backgroundColor = 'green';
+		e.target.classList.toggle('black');
+		e.target.classList.toggle('purple');
 	});
 
 	viewThree.addEventListener('click', (e) => {
-		e.stopPropagation();
 		e.target.style.backgroundColor = 'purple';
 	});
 
 	h2.addEventListener('click', (e) => {
-		e.target.textContent = 'Heading clicked';
+		const myText = e.target.textContent;
+		myText === 'My 2nd view' ? (e.target.textContent = 'Clicked') : (e.target.textContent = 'My 2nd view');
+	});
+
+	nav.addEventListener('mouseover', (e) => {
+		e.target.classList.add('height100');
+	});
+
+	nav.addEventListener('mouseout', (e) => {
+		e.target.classList.remove('height100');
 	});
 };
