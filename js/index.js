6ce015944a0be4d69fe14075a2d72669b1af3e75
Radosław Siek <Borderlandsmaniak@gmail.com>
2023-04-12 16:18:27 +0200

    12.04

--- js/index.js
+++ js/index.js
@@ -3,24 +3,51 @@
 
 // workflow function
 
-const getAllUserEmails = async () => {
-	const res = await fetch('https://jsonplaceholder.typicode.com/users');
-	const jsonData = await res.json();
+// const getAllUserEmails = async () => {
+// 	const res = await fetch('https://jsonplaceholder.typicode.com/users');
+// 	const jsonData = await res.json();
 
-	const userEmailArray = jsonData.map((user) => user.email);
+// 	const userEmailArray = jsonData.map((user) => user.email);
 
-	renderUserEmails(userEmailArray);
-};
+// 	renderUserEmails(userEmailArray);
+// };
 
-const renderUserEmails = (data) => {
-	data.forEach((email) => {
-		const p = document.createElement('p');
-		p.classList.add('text-style');
-		const pText = document.createTextNode(email);
-		p.appendChild(pText);
+// const renderUserEmails = (data) => {
+// 	data.forEach((email) => {
+// 		const p = document.createElement('p');
+// 		p.classList.add('text-style');
+// 		const pText = document.createTextNode(email);
+// 		p.appendChild(pText);
 
-		document.body.appendChild(p);
+// 		document.body.appendChild(p);
+// 	});
+// };
+
+// getAllUserEmails();
+
+// 2nd parameter of fetch is a object
+
+const getDadJoke = async () => {
+	const res = await fetch('https://icanhazdadjoke.com', {
+		method: 'GET',
+		headers: {
+			Accept: 'application/json',
+		},
 	});
+	const jsonDadJoke = await res.json();
+
+	logJoke(jsonDadJoke.joke);
 };
 
-getAllUserEmails();
+const logJoke = (joke) => console.log(joke);
+
+getDadJoke();
+
+const requestJoke = async (firstName, lastName) => {
+	const res = await fetch(`http://api.icndb.com/jokes/random?firstName=${firstName}&lastName=${lastName}`);
+	const jsonResponse = await res.json();
+
+	console.log(jsonResponse.value);
+};
+
+requestJoke('Emilia', 'Kożuch');
