0f27fdbd9cb0a7cee2482553ec0b74c8c4ddef51
Radosław Siek <Borderlandsmaniak@gmail.com>
2023-04-12 14:01:48 +0200

    12.04

--- js/index.js
+++ js/index.js
@@ -5,13 +5,34 @@
 
 // 3 steps: Pending, Resolve, Reject
 
-const users = fetch('https://jsonplaceholder.typicode.com/users');
+// Async / Await
 
-// Pending
-users
-	.then((res) => res.json())
-	.then((users) => {
-		users.forEach((user) => {
-			console.log(user.name);
-		});
+const myUser = {
+	userList: [],
+};
+
+const myCoolFunction = async () => {
+	const response = await fetch('https://jsonplaceholder.typicode.com/users');
+	const jsonUserData = await response.json();
+	return jsonUserData;
+};
+
+myCoolFunction();
+
+const anotherFunction = async () => {
+	const data = await myCoolFunction();
+	console.log(data);
+	return data;
+};
+
+anotherFunction();
+
+const insertingUsers = async () => {
+	const users = await anotherFunction();
+	users.forEach((user) => {
+		myUser.userList.push(user);
 	});
+	console.log(myUser.userList);
+};
+
+insertingUsers();
