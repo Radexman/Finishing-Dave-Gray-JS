41b6a283dc33c9ac769bbf33596544c7919a32f4
Radosław Siek <Borderlandsmaniak@gmail.com>
2023-04-12 13:29:52 +0200

    12.04

--- js/index.js
+++ js/index.js
@@ -5,26 +5,13 @@
 
 // 3 steps: Pending, Resolve, Reject
 
-const promise = new Promise((resolve, reject) => {
-	let error = true;
+const users = fetch('https://jsonplaceholder.typicode.com/users');
 
-	if (!error) {
-		resolve('Success: Task compleated.');
-	} else {
-		reject('Error: Something went wrong.');
-	}
-});
-
-const myNextPromise = new Promise((resolve, reject) => {
-	setTimeout(() => {
-		const error = false;
-
-		if (!error) {
-			resolve('Sucess!');
-		} else {
-			reject('Failure!');
-		}
-	}, 1500);
-});
-
-myNextPromise.then((res) => console.log(res)).catch((error) => console.log(error));
+// Pending
+users
+	.then((res) => res.json())
+	.then((users) => {
+		users.forEach((user) => {
+			console.log(user.name);
+		});
+	});
